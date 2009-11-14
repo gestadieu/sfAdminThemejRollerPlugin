17324f59f8d4d177373ed08ce73e0347141a67bd
Jeremy <jeremyb@macbook-de-jeremy-barthe.local>
2009-11-14 12:03:16 +0100

    improved display of flashs messages (with icons & stylesheet)

--- data/generator/sfDoctrineModule/jroller/template/templates/_flashes.php
+++ data/generator/sfDoctrineModule/jroller/template/templates/_flashes.php
@@ -1,9 +1,15 @@
-<div class="sf_admin_flashes">
+<div class="sf_admin_flashes ui-widget">
 [?php if ($sf_user->hasFlash('notice')): ?]
-  <div class="ui-widget"><div class="notice ui-state-highlight ui-corner-all">[?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]</div></div>
+  <div class="notice ui-state-highlight ui-corner-all">
+    <span class="ui-icon ui-icon-info floatleft"></span>&nbsp;
+    [?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]
+  </div>
 [?php endif; ?]
 
 [?php if ($sf_user->hasFlash('error')): ?]
-  <div class="ui-widget"><div class="error ui-state-error ui-corner-all">[?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]</div></div>
+  <div class="error ui-state-error ui-corner-all">
+    <span class="ui-icon ui-icon-alert floatleft"></span>&nbsp;
+    [?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]
+  </div>
 [?php endif; ?]
 </div>
