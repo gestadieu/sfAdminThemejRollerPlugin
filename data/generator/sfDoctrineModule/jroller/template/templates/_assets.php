a929826b21a9cd23bbfe17843508a5aa4bd483e7
Jeremy <jeremyb@macbook-de-jeremy-barthe.local>
2009-11-15 13:36:21 +0100

    jquery should be loaded first, before any other js library (issue #3) (idem for stylesheets)

--- data/generator/sfDoctrineModule/jroller/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/jroller/template/templates/_assets.php
@@ -1,28 +1,28 @@
 <?php if (sfConfig::get('app_sf_admin_theme_jroller_plugin_css_reset')): // reset css ?>
-  [?php use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/reset.css'?>') ?]
+  [?php use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/reset.css' ?>', 'first') ?]
 <?php endif; ?>
 
 <?php if (sfConfig::get('app_sf_admin_theme_jroller_plugin_use_jquery')): // use jQuery ?>
-  [?php use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/jquery.min.js'?>') ?]
-  [?php use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/jquery-ui.custom.min.js'?>') ?]
+  [?php use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/jquery.min.js' ?>', 'first') ?]
+  [?php use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/jquery-ui.custom.min.js' ?>', 'first') ?]
 <?php endif; ?>
 
-[?php use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_theme_dir','/sfAdminThemejRollerPlugin/css/jquery').'/' . sfConfig::get('app_sf_admin_theme_jroller_plugin_theme','redmond').'/jquery-ui.custom.css'?>') ?]
+[?php use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_theme_dir','/sfAdminThemejRollerPlugin/css/jquery').'/' . sfConfig::get('app_sf_admin_theme_jroller_plugin_theme','redmond').'/jquery-ui.custom.css' ?>', 'first') ?]
 
 <?php if (isset($this->params['css'])): // custom CSS ?>
   [?php use_stylesheet('<?php echo $this->params['css'] ?>', 'first') ?]
 <?php else: // jRoller CSS ?>
-  [?php use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/jroller.css'?>') ?]
+  [?php use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/jroller.css' ?>', 'first') ?]
 <?php endif; ?>
 
 [?php // additionnal stylesheet (filament group)
-  use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/fg.menu.css'?>');
-  use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/fg.buttons.css'?>');
-  use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/ui.selectmenu.css'?>');
+  use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/fg.menu.css' ?>', 'first');
+  use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/fg.buttons.css' ?>', 'first');
+  use_stylesheet('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin'). '/css/ui.selectmenu.css' ?>', 'first');
 ?]
 
 [?php // additionnal javascript (filament group)
-  use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/fg.menu.js'?>');
-  use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/jroller.js'?>');
-  use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/ui.selectmenu.js'?>');
+  use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/fg.menu.js' ?>', 'first');
+  use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/jroller.js' ?>', 'first');
+  use_javascript('<?php echo sfConfig::get('app_sf_admin_theme_jroller_plugin_web_dir','/sfAdminThemejRollerPlugin') . '/js/ui.selectmenu.js' ?>', 'first');
 ?]
